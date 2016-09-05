92cf736e963849d21774d2bf4032386c8fcda349
Orgenus <obasli@gmail.com>
2016-09-05 18:13:03 +0300

    Add files via upload

--- src/Payu.php
+++ src/Payu.php
@@ -6,7 +6,127 @@
  * Date: 5.09.2016
  * Time: 13:58
  */
+
+use PayU\Alu\Billing;
+use PayU\Alu\Card;
+use PayU\Alu\Client;
+use PayU\Alu\Delivery;
+use PayU\Alu\MerchantConfig;
+use PayU\Alu\Order;
+use PayU\Alu\Product;
+use PayU\Alu\Request;
+use PayU\Alu\User;
+use PayU\Alu\Exceptions\ConnectionException;
+use PayU\Alu\Exceptions\ClientException;
+
+
 class Payu
 {
+    private $aluCfg;
+    private $aluUser;
+    private $aluOrder;
+    private $aluBilling;
+    private $aluDelivery;
+    private $aluCard;
+    private $aluRequest;
+    private $aluClient;
+
+    public function __construct($county = "TR", $timeOut = 1000, $paymentMethod = "CCVISAMC")
+    {
+        $this->aluCfg = new MerchantConfig(config('payu.MERCHANT_ID'), config('payu.SECRET'), $county);
+        $this->aluUser = new User(\Illuminate\Http\Request::getClientIP());
+        $this->aluOrder = new Order();
+        $this->aluBilling = new Billing();
+        $this->aluDelivery = new Delivery();
+
+
+
+        $this->aluOrder->withOrderTimeout($timeOut);
+        $this->aluOrder->withPayMethod($paymentMethod);
+
+    }
+
+    public function set3DSReturn($url)
+    {
+        $this->aluOrder->withBackRef($url);
+    }
+
+    public function setOrderRef($s)
+    {
+        $this->aluOrder->withOrderRef($s);
+    }
+
+    public function setCurrency($s)
+    {
+        $this->aluOrder->withCurrency($s);
+    }
+
+    public function setOrderDate($s)
+    {
+        $this->aluOrder->withOrderDate(date('Y-m-d H:i:s', strtotime($s)));
+    }
+
+   public function addProduct($code, $name, $price, $vat, $quantity)
+   {
+       $product = new Product();
+       $product->withCode($code)
+           ->withName($name)
+           ->withPrice($price)
+           ->withVAT($vat)
+           ->withQuantity($quantity);
+       $this->aluOrder->addProduct($product);
+
+   }
+
+   public function setBilling($address1, $address2, $city, $county, $email, $firstname, $lastname, $phone, $identityNumber)
+   {
+       $this->aluBilling->withAddressLine1($address1)
+           ->withAddressLine2($address2)
+           ->withCity($city)
+           ->withCountryCode($county)
+           ->withEmail($email)
+           ->withFirstName($firstname)
+           ->withLastName($lastname)
+           ->withPhoneNumber($phone)
+           ->withIdentityCardNumber($identityNumber);
+   }
+
+    public function setDelivery($address1, $address2, $city, $county, $email, $firstname, $lastname, $phone)
+    {
+        $this->aluDelivery->withAddressLine1($address1)
+            ->withAddressLine2($address2)
+            ->withCity($city)
+            ->withCountryCode($county)
+            ->withEmail($email)
+            ->withFirstName($firstname)
+            ->withLastName($lastname)
+            ->withPhoneNumber($phone);
+    }
+    public function setCard($cardNumber, $cardExpMo, $cardExpYe, $cardCCV, $cardOwn)
+    {
+        $this->aluCard = new Card($cardNumber, $cardExpMo, $cardExpYe, $cardCCV, $cardOwn);
+    }
+
+    public function buy()
+    {
+        $this->aluRequest = new Request($this->aluCfg, $this->aluOrder, $this->aluBilling, $this->aluDelivery, $this->aluUser);
+        $this->aluRequest->setCard($this->aluCard);
+
+        $this->aluClient = new Client($this->aluCfg);
+        try {
+
+            $response = $this->aluClient->pay($this->aluRequest);
+
+            if ($response->isThreeDs()) {
+                return ['error' => false, 'threeDS' => $response->getThreeDsUrl()];
+            }
+            return ['error' => false, 'status' => $response->getStatus(), 'returnCode'=> $response->getReturnCode(), 'returnMessage'=> $response->getReturnMessage()];
+        } catch (ConnectionException $exception) {
+            return ['error' => true, 'returnMessage'=> $exception->getMessage()];
+        } catch (ClientException $exception) {
+            return ['error' => true, 'returnMessage'=> $exception->getErrorMessage()];
+        }
+
+    }
 
 }
