da5c8945abc0edf73c593d8e095d811580acc3e2
Orgenus <obasli@gmail.com>
2016-09-09 14:54:40 +0300

    Update Payu.php

--- src/Payu.php
+++ src/Payu.php
@@ -6,6 +6,24 @@ namespace Orgenus\Payu;
  * Date: 5.09.2016
  * Time: 13:58
  */
+ 
+require_once dirname(__FILE__) . '/PayU/Alu/Exceptions/ClientException.php';
+require_once dirname(__FILE__) . '/PayU/Alu/Exceptions/ConnectionException.php';
+require_once dirname(__FILE__) . '/PayU/Alu/AbstractCommonAddress.php';
+require_once dirname(__FILE__) . '/PayU/Alu/MerchantConfig.php';
+require_once dirname(__FILE__) . '/PayU/Alu/User.php';
+require_once dirname(__FILE__) . '/PayU/Alu/HashService.php';
+require_once dirname(__FILE__) . '/PayU/Alu/HTTPClient.php';
+require_once dirname(__FILE__) . '/PayU/Alu/Card.php';
+require_once dirname(__FILE__) . '/PayU/Alu/CardToken.php';
+require_once dirname(__FILE__) . '/PayU/Alu/Billing.php';
+require_once dirname(__FILE__) . '/PayU/Alu/Delivery.php';
+require_once dirname(__FILE__) . '/PayU/Alu/Product.php';
+require_once dirname(__FILE__) . '/PayU/Alu/Order.php';
+require_once dirname(__FILE__) . '/PayU/Alu/Request.php';
+require_once dirname(__FILE__) . '/PayU/Alu/Response.php';
+require_once dirname(__FILE__) . '/PayU/Alu/ResponseWireAccount.php';
+require_once dirname(__FILE__) . '/PayU/Alu/Client.php';
 
 use PayU\Alu\Billing;
 use PayU\Alu\Card;
